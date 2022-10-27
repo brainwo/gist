ad56b8be5c75347370f98f6532f0efaf26a56f40
Brian Wo <45139213+brainwo@users.noreply.github.com>
2022-10-27 21:06:51 +0800

    update Thu Oct 27 09:06:51 PM +08 2022

---
+++ change-term-color.lua
@@ -0,0 +1,14 @@
+-- ~/.config/mpv/scripts/change-term-color.lua
+
+local mp = require 'mp'
+local os = require 'os'
+
+local function my_fn(event)
+    print("start of playback!")
+    if (event.reason == "quit")
+    then
+	os.execute("kitty @ set-colors -a background=#1f2d3a")
+    end
+end
+
+mp.register_event("end-file", my_fn)
