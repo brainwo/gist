50dc901a1af3bd3b51f3c2fdb5ea41091fc9b8c0
Brian Wo <45139213+brainwo@users.noreply.github.com>
2023-10-18 11:40:19 +0800

    update Wed Oct 18 11:40:19 AM +08 2023

--- change-term-color.lua
+++ change-term-color.lua
@@ -4,10 +4,10 @@ local mp = require 'mp'
 local os = require 'os'
 
 local function my_fn(event)
-    if (event.reason == "quit")
-    then
-    	endos.execute("kitty @ set-colors -a background=#1f2d3a")
-    end
+	if (event.reason == "quit")
+	then
+		endos.execute("kitty @ set-colors -a background=#1f2d3a")
+	end
 end
 
 mp.register_event("end-file", my_fn)
